66eb1a5698e69333583df397a17e4e5d44c3c701
Iron <iron.yehong@outlook.com>
2018-06-23 22:14:47 +0800

    Initialize with demo of Hosting WPF in Win32.

---
+++ Codes/WpfControl/MyControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace WpfControl
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class MyControl
+    {
+        public MyControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MessageButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello, I'm from WPF.");
+        }
+
+
+        private void WindowButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            new MyWindow().ShowDialog();
+        }
+    }
+}
---
+++ Codes/WpfControl/MyWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+
+namespace WpfControl
+{
+    /// <summary>
+    /// Interaction logic for MyWindow.xaml
+    /// </summary>
+    public partial class MyWindow
+    {
+        public MyWindow()
+        {
+            InitializeComponent();
+
+            // double click mouse to close this window
+            MouseDoubleClick += (s, e) =>
+            {
+                Close();
+            };
+
+            // press and drag left mouse button to move this window
+            MouseLeftButtonDown += (s, e) =>
+            {
+                if (e.ButtonState == MouseButtonState.Pressed)
+                {
+                    DragMove();
+                }
+            };
+        }
+    }
+}
